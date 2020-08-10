e5935df6be3ac63af3c247c3bb1c366277626902
josuecedeno <josuecedeno@yahoo.com>
2020-08-10 14:47:07 -0500

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,67 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-export class CampsiteInfo extends Component {
-  //Error received 'no-useless-constructor
-  // constructor(props) {
-  //   super(props);
-  // }
+function RenderCampsite({ image, name, description }) {
+  return (
+    <div className='col-md-5 m-1'>
+      <Card>
+        <CardImg top src={image} alt={name} />
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <CardText>{description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-  renderCampsite({ image, name, description }) {
+function RenderComments(comments) {
+  if (comments) {
     return (
       <div className='col-md-5 m-1'>
-        <Card>
-          <CardImg top src={image} alt={name} />
-          <CardBody>
-            <CardTitle>{name}</CardTitle>
-            <CardText>{description}</CardText>
-          </CardBody>
-        </Card>
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return (
+            <>
+              <p>{comment.text}</p>
+              <p>
+                -- {comment.author}{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit',
+                }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </>
+          );
+        })}
       </div>
     );
   }
 
-  renderComments(comments) {
-    if (comments) {
-      return (
-        <div className='col-md-5 m-1'>
-          <h4>Comments</h4>
-          {comments.map((comment) => {
-            return (
-              <>
-                <p>{comment.text}</p>
-                <p>
-                  -- {comment.author}{' '}
-                  {new Intl.DateTimeFormat('en-US', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: '2-digit',
-                  }).format(new Date(Date.parse(comment.date)))}
-                </p>
-              </>
-            );
-          })}
-        </div>
-      );
-    }
+  return <div />;
+}
 
-    return <div />;
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          {RenderCampsite(props.campsite)}
+          {RenderComments(props.campsite.comments)}
+        </div>
+      </div>
+    );
   }
 
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div className='container'>
-          <div className='row'>
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-          </div>
-        </div>
-      );
-    }
-
-    return <div />;
-  }
+  return <div />;
 }
 
 export default CampsiteInfo;
